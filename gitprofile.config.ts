132299e1d9bff8ad2b21f3f8956f6c2a18377ec7
quinn <154846367+quinn6957@users.noreply.github.com>
2025-07-23 04:34:50 +0000

    attempt #3

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -9,7 +9,7 @@ const CONFIG = {
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/eitgen.github.io/',
+  base: '/eitgen.org/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
