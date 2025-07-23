52089d35eae1b5be7edcc4ba9d734f36cfa31767
quinn <154846367+quinn6957@users.noreply.github.com>
2025-07-23 04:11:37 +0000

    Make portfolio from gitprofile.

---
+++ gitprofile.config.ts
@@ -0,0 +1,140 @@
+// gitprofile.config.ts
+
+const CONFIG = {
+  github: {
+    username: 'quinn6957', // Your GitHub org/user name. (This is the only required config)
+  },
+  /**
+   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
+   * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
+   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
+   */
+  base: '/',
+  projects: {
+    github: {
+      display: true, // Display GitHub projects?
+      header: 'Github Projects',
+      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      automatic: {
+        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
+        limit: 8, // How many projects to display.
+        exclude: {
+          forks: false, // Forked projects will not be displayed if set to true.
+          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        },
+      },
+      manual: {
+        // Properties for manually specifying projects
+        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+      },
+    },
+  },
+  seo: { title: 'Portfolio of Quinn Smith', description: '', imageURL: '' },
+  social: {
+    linkedin: '',
+    x: 'vByte198496',
+    mastodon: '',
+    researchGate: '',
+    facebook: '',
+    instagram: '',
+    reddit: '',
+    threads: '',
+    youtube: '', // example: 'pewdiepie'
+    udemy: '',
+    dribbble: '',
+    behance: '',
+    medium: '',
+    dev: '',
+    stackoverflow: '', // example: '1/jeff-atwood'
+    discord: '',
+    telegram: '',
+    website: 'https://eitgen.org',
+    phone: '',
+    email: 'quinn@eitgen.org',
+  },
+  resume: {
+    fileUrl:
+      '', // Empty fileUrl will hide the `Download Resume` button.
+  },
+  skills: [
+    'C#',
+    'Git',
+  ],
+  educations: [
+    {
+      institution: 'Lanphier High School',
+      degree: 'High School Diploma',
+      from: '2024',
+      to: 'Present',
+    },
+    {
+      institution: 'Normal West High School',
+      degree: 'None',
+      from: '2022',
+      to: '2024',
+    },
+  ],
+  googleAnalytics: {
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  },
+  // Track visitor interaction and behavior. https://www.hotjar.com
+  hotjar: { id: '', snippetVersion: 6 },
+  themeConfig: {
+    defaultTheme: 'halloween',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: true,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Display the ring in Profile picture
+    displayAvatarRing: true,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset',
+      'caramellatte',
+      'abyss',
+      'silk',
+      'procyon',
+    ],
+  },
+
+  enablePWA: true,
+};
+
+export default CONFIG;
